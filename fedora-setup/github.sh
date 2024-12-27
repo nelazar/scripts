d48fc98ba7a62b0cd07e82d908a3dde652d54c85
Noah Elazar <noahbelazar@gmail.com>
2024-12-26 21:14:52 -0500

    Created github script

---
+++ fedora-setup/github.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+# Read variables
+email=$(<../secrets/email.txt)
+username=$(<../secrets/username.txt)
+
+# Setup ssh
+ssh-keygen -t ed25519 -C "$email" -f "~/.ssh/id_ed25519" -N ""
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
+echo "Add below key to github:"
+cat ~/.ssh/id_ed25519.pub
+
+# Configure git
+git config --global user.email "$email"
+git config --global user.name "$username"
---
+++ fedora-setup/secrets.sh
@@ -0,0 +1,5 @@
+# Creates the secrets directory and essential files if they don't already exist
+cd ..
+mkdir -p secrets
+touch email.txt
+touch username.txt
