17b0a0cbe343b673817b5ad5068b408baaa1ec27
Noah Elazar <noahbelazar@gmail.com>
2024-12-26 23:09:48 -0500

    Added folders script

---
+++ fedora-setup/folders.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+# Rename folders to be lowercase
+mv ~/Documents/ ~/documents/
+mv ~/Downloads/ ~/downloads/
+mv ~/Music/ ~/music/
+mv ~/Pictures/ ~/pictures/
+mv ~/videos/ ~/videos/
+
+# Delete unnecessary folders
+rm -r ~/Desktop/ ~/Public/ ~/Templates/
